c8a7a0b6e0a0821a51457d31de47e959bfba91cf
rnqhstmd <rnqhstmd0219@gmail.com>
2023-11-03 14:16:42 +0900

    user 스웨거 시도중

--- src/main/java/com/ssafy/enjoytrip/user/controller/UserController.java
+++ src/main/java/com/ssafy/enjoytrip/user/controller/UserController.java
@@ -2,8 +2,10 @@ package com.ssafy.enjoytrip.user.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,4 +43,40 @@ public class UserController {
 		session.invalidate();
 		return "redirect:/";
 	}
+	
+	@GetMapping
+	public String update() {
+		return "user/update";
+	}
+	
+	@PutMapping
+	public String update(User user) {
+		String hashPwd = pwdhash.getHasing(user.getUserId(), user.getUserPassword());
+		user.setUserPassword(hashPwd);
+		service.update(user);
+		// 회원 정보 보여주는 창으로 이동
+		return "redirect:/user/info";
+	}
+	
+//	@GetMapping
+//	public String regist() {
+//		return "user/regist";
+//	}
+	
+	@PostMapping
+	public String regist(User user) {
+		String hashPwd = pwdhash.getHasing(user.getUserId(), user.getUserPassword());
+		user.setUserPassword(hashPwd);
+		service.regist(user);
+		// 메인 페이지로 이동
+		return "redirect:/";
+	}
+	
+	@DeleteMapping
+	public String exit(String user_id) {
+		service.exit(user_id);
+		// 메인 페이지로 이동
+		return "redirect:/";
+	}
+	
 }
