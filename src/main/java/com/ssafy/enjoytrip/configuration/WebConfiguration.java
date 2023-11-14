ceeb28ea32687d656e0dc955c12bcc34b1393098
SMY <ztr@naver.com>
2023-11-14 23:52:49 +0900

    Fix uri and qnaBoard Mapper

--- src/main/java/com/ssafy/enjoytrip/configuration/WebConfiguration.java
+++ src/main/java/com/ssafy/enjoytrip/configuration/WebConfiguration.java
@@ -1,13 +1,18 @@
 package com.ssafy.enjoytrip.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfiguration implements WebMvcConfigurer  {
 	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
+		// addMapping("/**") is not allowed:
+		//	When allowCredentials is true, allowedOrigins cannot contain the special value "*" since that cannot be set on the "Access-Control-Allow-Origin" response header.
+		//	To allow credentials to a set of origins, list them explicitly or consider using "allowedOriginPatterns" instead.
+		registry.addMapping("/enjoytrip")
 			.allowedOrigins("http://localhost:5173", "http://localhost:5174") // 허용할 출처
 			.allowedMethods("GET", "POST", "PUT", "DELETE")
 			.allowCredentials(true)
